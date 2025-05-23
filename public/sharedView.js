a0996ccaedad9ccc606e2f749e544a8875fca781
rossorudolph <rossorudolph@gmail.com>
2025-05-22 20:31:22 -0700

    new viz

--- public/sharedView.js
+++ public/sharedView.js
@@ -133,3 +133,106 @@ function touchStarted(e) {
 
 /* leave this here so that Glitch will not mark global p5.js functions as errors */
 /* globals io, ADD, ALT, ARROW, AUDIO, AUTO, AXES, BACKSPACE, BASELINE, BEVEL, BEZIER, BLEND, BLUR, BOLD, BOLDITALIC, BOTTOM, BURN, CENTER, CHORD, CLAMP, CLOSE, CONTROL, CORNER, CORNERS, CROSS, CURVE, DARKEST, DEGREES, DEG_TO_RAD, DELETE, DIFFERENCE, DILATE, DODGE, DOWN_ARROW, ENTER, ERODE, ESCAPE, EXCLUSION, FALLBACK, FILL, GRAY, GRID, HALF_PI, HAND, HARD_LIGHT, HSB, HSL, IMAGE, IMMEDIATE, INVERT, ITALIC, LABEL, LANDSCAPE, LEFT, LEFT_ARROW, LIGHTEST, LINEAR, LINES, LINE_LOOP, LINE_STRIP, MIRROR, MITER, MOVE, MULTIPLY, NEAREST, NORMAL, OPAQUE, OPEN, OPTION, OVERLAY, P2D, PI, PIE, POINTS, PORTRAIT, POSTERIZE, PROJECT, QUADRATIC, QUADS, QUAD_STRIP, QUARTER_PI, RADIANS, RADIUS, RAD_TO_DEG, REMOVE, REPEAT, REPLACE, RETURN, RGB, RIGHT, RIGHT_ARROW, ROUND, SCREEN, SHIFT, SOFT_LIGHT, SQUARE, STROKE, SUBTRACT, TAB, TAU, TESS, TEXT, TEXTURE, THRESHOLD, TOP, TRIANGLES, TRIANGLE_FAN, TRIANGLE_STRIP, TWO_PI, UP_ARROW, VIDEO, WAIT, WEBGL, accelerationX, accelerationY, accelerationZ, deltaTime, deviceOrientation, displayHeight, displayWidth, focused, frameCount, height, isKeyPressed, key, keyCode, keyIsPressed, mouseButton, mouseIsPressed, mouseX, mouseY, movedX, movedY, pAccelerationX, pAccelerationY, pAccelerationZ, pRotateDirectionX, pRotateDirectionY, pRotateDirectionZ, pRotationX, pRotationY, pRotationZ, pixels, pmouseX, pmouseY, pwinMouseX, pwinMouseY, rotationX, rotationY, rotationZ, touches, turnAxis, width, winMouseX, winMouseY, windowHeight, windowWidth, abs, acos, alpha, ambientLight, ambientMaterial, angleMode, append, applyMatrix, arc, arrayCopy, asin, atan, atan2, background, beginContour, beginShape, bezier, bezierDetail, bezierPoint, bezierTangent, bezierVertex, blend, blendMode, blue, boolean, box, brightness, byte, camera, ceil, char, circle, clear, clearStorage, color, colorMode, concat, cone, constrain, copy, cos, createA, createAudio, createButton, createCamera, createCanvas, createCapture, createCheckbox, createColorPicker, createDiv, createElement, createFileInput, createGraphics, createImage, createImg, createInput, createNumberDict, createP, createRadio, createSelect, createShader, createSlider, createSpan, createStringDict, createVector, createVideo, createWriter, cursor, curve, curveDetail, curvePoint, curveTangent, curveTightness, curveVertex, cylinder, day, debugMode, degrees, describe, describeElement, directionalLight, displayDensity, dist, downloadFile, ellipse, ellipseMode, ellipsoid, emissiveMaterial, endContour, endShape, erase, exitPointerLock, exp, fill, filter, float, floor, fract, frameRate, frustum, fullscreen, get, getFrameRate, getItem, getURL, getURLParams, getURLPath, green, gridOutput, hex, hour, httpDo, httpGet, httpPost, hue, image, imageMode, int, isLooping, join, keyIsDown, lerp, lerpColor, lightFalloff, lightness, lights, line, loadBytes, loadFont, loadImage, loadJSON, loadModel, loadPixels, loadShader, loadStrings, loadTable, loadXML, log, loop, mag, map, match, matchAll, max, millis, min, minute, model, month, nf, nfc, nfp, nfs, noCanvas, noCursor, noDebugMode, noErase, noFill, noLights, noLoop, noSmooth, noStroke, noTint, noise, noiseDetail, noiseSeed, norm, normalMaterial, orbitControl, ortho, perspective, pixelDensity, plane, point, pointLight, pop, popMatrix, popStyle, pow, print, push, pushMatrix, pushStyle, quad, quadraticVertex, radians, random, randomGaussian, randomSeed, rect, rectMode, red, redraw, registerPromisePreload, removeElements, removeItem, requestPointerLock, resetMatrix, resetShader, resizeCanvas, reverse, rotate, rotateX, rotateY, rotateZ, round, saturation, save, saveCanvas, saveFrames, saveGif, saveJSON, saveJSONArray, saveJSONObject, saveStrings, saveTable, scale, second, select, selectAll, set, setAttributes, setCamera, setFrameRate, setMoveThreshold, setShakeThreshold, shader, shearX, shearY, shininess, shorten, shuffle, sin, smooth, sort, specularColor, specularMaterial, sphere, splice, split, splitTokens, spotLight, sq, sqrt, square, storeItem, str, stroke, strokeCap, strokeJoin, strokeWeight, subset, tan, text, textAlign, textAscent, textDescent, textFont, textLeading, textOutput, textSize, textStyle, textWidth, texture, textureMode, textureWrap, tint, torus, translate, triangle, trim, unchar, unhex, updatePixels, vertex, writeFile, year */
+/**
+ * Add a "balancing ball" with a trail for each user.
+ * Each user gets their own ball and trail, updated based on their device orientation.
+ */
+
+// Store ball state for each user: position and trail
+const userBalls = new Map();
+
+function updateUserBall(user) {
+  // Initialize ball state if not present
+  if (!userBalls.has(user.id)) {
+    // Start ball in the center of the user's window
+    userBalls.set(user.id, {
+      cx: user.windowWidth / 2 || width / 2,
+      cy: user.windowHeight / 2 || height / 2,
+      trail: []
+    });
+  }
+  const ball = userBalls.get(user.id);
+
+  // Use device orientation to move the ball
+  const dx = constrain(user.rotationY || 0, -3, 3);
+  const dy = constrain(user.rotationX || 0, -3, 3);
+  ball.cx += dx * 2;
+  ball.cy += dy * 2;
+
+  // Constrain to user's window size
+  ball.cx = constrain(ball.cx, 0, user.windowWidth * 0.25 || width * 0.25);
+  ball.cy = constrain(ball.cy, 0, user.windowHeight * 0.25 || height * 0.25);
+
+  // Add to trail
+  ball.trail.push({ x: ball.cx, y: ball.cy });
+  if (ball.trail.length > 100) ball.trail.shift();
+}
+
+// Patch draw() to add the ball and trail for each user
+const originalDraw = draw;
+draw = function() {
+  background(51);
+
+  const radius = min(width/2, height/2) * .8;
+  let i = 0;
+
+  for(const [id, user] of users.entries()) {
+    const angle = map(i, 0, users.size, 0, 360);
+    i++;
+    const x = radius * cos(angle);
+    const y = radius * sin(angle);
+
+    push();
+    translate(x, y);
+
+    if (user.name != undefined) {
+      fill(255);
+      text(user.name, 0, 20);
+
+      push();
+      noStroke();
+      rotateZ(user.rotationZ);
+      rotateX(user.rotationX);
+      rotateY(user.rotationY);
+
+      // x, y, z axes
+      push(); fill(255, 0, 0); translate(50, 0, 0); box(100, 5, 5); pop();
+      push(); fill(0, 255, 0); translate(0, 50, 0); box(5, 100, 5); pop();
+      push(); fill(0, 0, 255); translate(0, 0, 50); box(5, 5, 100); pop();
+
+      pop();
+
+      strokeWeight(5);
+      stroke(255);
+
+      const sca = .25;
+      translate(-user.windowWidth/2 * sca, -user.windowHeight/2 * sca);
+      for (const touch of user.touches) {
+        point(touch.x * sca, touch.y * sca, 0);
+      }
+      point(user.mouseX * sca, user.mouseY * sca);
+
+      // --- Balancing Ball with Trail ---
+      updateUserBall({ ...user, id });
+      const ball = userBalls.get(id);
+
+      // Draw trail
+      noFill();
+      stroke(255, 200, 0, 120);
+      beginShape();
+      for (const p of ball.trail) {
+        vertex(p.x * sca, p.y * sca, 1);
+      }
+      endShape();
+
+      // Draw ball
+      noStroke();
+      fill(255, 200, 0);
+      push();
+      translate(ball.cx * sca, ball.cy * sca, 2);
+      sphere(30 * sca, 24, 16);
+      pop();
+    }
+    pop();
+  }
+};
